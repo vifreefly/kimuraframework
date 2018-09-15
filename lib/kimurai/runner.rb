acff381e1c7e65e44b0a696bda4ae8506dfecedc
Victor Afanasev <vicfreefly@gmail.com>
2018-09-15 11:40:38 +0400

    Refactor Runner

--- lib/kimurai/runner.rb
+++ lib/kimurai/runner.rb
@@ -2,58 +2,37 @@ require 'pmap'
 
 module Kimurai
   class Runner
-    attr_reader :jobs, :spiders
+    attr_reader :jobs, :spiders, :session_info
 
     def initialize(parallel_jobs:)
       @jobs = parallel_jobs
       @spiders = Kimurai.list
+      @start_time = Time.now
+
+      @session_info = {
+        id: @start_time.to_i,
+        status: :processing,
+        start_time: @start_time,
+        stop_time: nil,
+        environment: Kimurai.env,
+        concurrent_jobs: @jobs,
+        spiders: @spiders.keys
+      }
 
       if time_zone = Kimurai.configuration.time_zone
         Kimurai.time_zone = time_zone
       end
+
+      ENV.store("SESSION_ID", @start_time.to_i.to_s)
+      ENV.store("RBCAT_COLORIZER", "false")
     end
 
-    def run!
-      start_time = Time.now
-      run_id = start_time.to_i
+    def run!(exception_on_fail: true)
       running_pids = []
 
-      ENV.store("RBCAT_COLORIZER", "false")
-
-      run_info = {
-        id: run_id,
-        status: :processing,
-        start_time: start_time,
-        stop_time: nil,
-        environment: Kimurai.env,
-        concurrent_jobs: jobs,
-        spiders: spiders.keys
-      }
-
-      at_exit do
-        # Prevent queue to process new intems while executing at_exit body
-        Thread.list.each { |t| t.kill if t != Thread.main }
-        # Kill currently running spiders
-        running_pids.each { |pid| Process.kill("INT", pid) }
-
-        error = $!
-        stop_time = Time.now
-
-        if error.nil?
-          run_info.merge!(status: :completed, stop_time: stop_time)
-        else
-          run_info.merge!(status: :failed, error: error.inspect, stop_time: stop_time)
-        end
-
-        if at_stop_callback = Kimurai.configuration.runner_at_stop_callback
-          at_stop_callback.call(run_info)
-        end
-        puts "<<< Runner: stopped: #{run_info}"
-      end
-
-      puts ">>> Runner: started: #{run_info}"
+      puts ">>> Runner: started: #{session_info}"
       if at_start_callback = Kimurai.configuration.runner_at_start_callback
-        at_start_callback.call(run_info)
+        at_start_callback.call(session_info)
       end
 
       spiders.peach_with_index(jobs) do |spider, i|
@@ -67,6 +46,21 @@ module Kimurai
         running_pids.delete(pid)
         puts "< Runner: stopped spider: #{spider_name}, index: #{i}"
       end
+    rescue StandardError, SignalException, SystemExit => e
+      session_info.merge!(status: :failed, error: e.inspect, stop_time: Time.now)
+      exception_on_fail ? raise(e) : [session_info, e]
+    else
+      session_info.merge!(status: :completed, stop_time: Time.now)
+    ensure
+      # Prevent queue to process new intems while executing at_exit body
+      Thread.list.each { |t| t.kill if t != Thread.main }
+      # Kill currently running spiders
+      running_pids.each { |pid| Process.kill("INT", pid) }
+
+      if at_stop_callback = Kimurai.configuration.runner_at_stop_callback
+        at_stop_callback.call(session_info)
+      end
+      puts "<<< Runner: stopped: #{session_info}"
     end
   end
 end

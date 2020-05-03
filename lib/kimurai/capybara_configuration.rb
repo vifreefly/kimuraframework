ffdc5f567e7262663d39c7970736519f4f8cf238
Victor Afanasev <vicfreefly@gmail.com>
2020-05-03 12:15:43 +0300

    Remove default type of selector :xpath from Capybara settings

--- lib/kimurai/capybara_configuration.rb
+++ lib/kimurai/capybara_configuration.rb
@@ -2,7 +2,6 @@ require 'capybara'
 
 Capybara.configure do |config|
   config.run_server = false
-  config.default_selector = :xpath
   config.save_path = "tmp"
   config.default_max_wait_time = 10
   config.ignore_hidden_elements = false

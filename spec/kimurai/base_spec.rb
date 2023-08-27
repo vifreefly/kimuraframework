takeyuwebinc/tanakai

write first tests with rspec

spec/tanakai/base_spec.rb
@@ -1,0 +1,133 @@
+RSpec.describe Tanakai::Base do 
+  describe '.running?' do
+    pending
+  end
+
+  describe '.completed?' do
+    pending
+  end
+
+  describe '.failed?' do
+    pending
+  end
+
+  describe '.visits' do
+    pending
+  end
+
+  describe '.items' do
+    pending
+  end
+
+  describe '.update' do
+    pending
+  end
+
+  describe '.add_event' do
+    pending
+  end
+
+  describe '.name' do
+    pending
+  end
+
+  describe '.engine' do
+    pending
+  end
+
+  describe '.pipelines' do
+    pending
+  end
+
+  describe '.start_urls' do
+    pending
+  end
+
+  describe '.config' do
+    pending
+  end
+
+  describe '.logger' do
+    pending
+  end
+
+  describe '.crawl!' do
+    pending
+  end
+
+  describe '.parse!' do
+    pending
+  end
+
+  describe '#browser' do
+    pending
+  end
+
+  describe '#request_to' do
+    pending
+  end
+
+  describe '#console' do
+    pending
+  end
+
+  describe '#storage' do
+    pending
+  end
+
+  describe '#unique?' do
+    pending
+  end
+
+  describe "#save_to" do
+    let(:item) { double('item') }
+    let(:path) { '/path/to/file' }
+    let(:format) { :csv }
+    let(:position) { true }
+    let(:append) { false }
+
+    context "when the Saver instance does not exist" do
+      it "creates a new Saver instance and stores it in the @savers instance variable" do
+        saver = instance_double(Tanakai::Base::Saver)
+        expect(saver).to receive(:save).with(item)
+        expect(Tanakai::Base::Saver).to receive(:new).with(path, format: format, position: position, append: append).and_return(saver)
+        allow(described_class).to receive(:savers).and_return({})
+
+        subject.save_to(path, item, format: format, position: position, append: append)
+
+        expect(subject.instance_variable_get(:@savers)[path]).to eq(saver)
+        expect(subject.class.savers[path]).to eq(nil)
+      end
+
+      context "when the with_info class method returns true" do
+        it "stores the Saver instance in the savers class variable" do
+          saver = instance_double(Tanakai::Base::Saver)
+          expect(saver).to receive(:save).with(item)
+          expect(subject).to receive(:with_info).and_return(true)
+          expect(Tanakai::Base::Saver).to receive(:new).with(path, format: format, position: position, append: append).and_return(saver)
+          allow(described_class).to receive(:savers).and_return({})
+
+          subject.save_to(path, item, format: format, position: position, append: append)
+
+          expect(subject.instance_variable_get(:@savers)[path]).to eq(saver)
+          expect(subject.class.savers[path]).to eq(saver)
+        end
+      end
+    end
+
+    context "when the Saver instance already exists" do
+      it "does not create a new Saver instance" do
+        saver = instance_double(Tanakai::Base::Saver)
+        expect(saver).to receive(:save).with(item)
+        subject.instance_variable_set(:@savers, { path => saver })
+        expect(Tanakai::Base::Saver).not_to receive(:new)
+        subject.save_to(path, item, format: format, position: position, append: append)
+      end
+    end
+  end
+
+
+  describe '#add_event' do
+    pending
+  end
+end

spec/tanakai/cli_spec.rb
@@ -1,0 +1,61 @@
+require 'tanakai/cli'
+
+RSpec.describe Tanakai::CLI do
+  describe "#generate" do
+    context "when generator_type is 'project'" do
+      it "generates a new project with the given name" do
+        expect_any_instance_of(Tanakai::CLI::Generator).to receive(:generate_project).with("foo")
+        subject.generate("project", "foo")
+      end
+
+      it "raises an error if no project name is provided" do
+        expect { subject.generate("project") }.to raise_error("Provide project name to generate a new project")
+      end
+    end
+
+    context "when generator_type is 'spider'" do
+      it "generates a new spider with the given name" do
+        expect_any_instance_of(Tanakai::CLI::Generator).to receive(:generate_spider).with("foo", in_project: false)
+        subject.generate("spider", "foo")
+      end
+
+      it "raises an error if no spider name is provided" do
+        expect { subject.generate("spider") }.to raise_error("Provide spider name to generate a spider")
+      end
+    end
+
+    context "when generator_type is 'schedule'" do
+      it "generates a new schedule" do
+        expect_any_instance_of(Tanakai::CLI::Generator).to receive(:generate_schedule)
+        subject.generate("schedule")
+      end
+    end
+
+    context "when generator_type is unknown" do
+      it "raises an error" do
+        expect { subject.generate("unknown") }.to raise_error("Don't know this generator type: unknown")
+      end
+    end
+  end
+
+  describe "#setup" do
+    it "sets up the server with the given options" do
+      builder = instance_double(Tanakai::CLI::AnsibleCommandBuilder)
+      expect(Tanakai::CLI::AnsibleCommandBuilder).to receive(:new).with("user_host", {}, playbook: "setup").and_return(builder)
+      expect(builder).to receive(:get).and_return("ls")
+      subject.setup("user_host")
+    end
+  end
+
+  describe "#deploy" do
+    pending
+  end
+
+  describe "#crawl" do
+    pending
+  end
+
+  describe "#parse" do
+    pending
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,102 @@
+require "bundler/setup"
+require "tanakai"
+require "pry-nav"
+
+# This file was generated by the `rspec --init` command. Conventionally, all
+# specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
+# The generated `.rspec` file contains `--require spec_helper` which will cause
+# this file to always be loaded, without a need to explicitly require it in any
+# files.
+#
+# Given that it is always loaded, you are encouraged to keep this file as
+# light-weight as possible. Requiring heavyweight dependencies from this file
+# will add to the boot time of your test suite on EVERY test run, even for an
+# individual file that may not need all of that loaded. Instead, consider making
+# a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need
+# it.
+#
+# See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
+  # have no way to turn it off -- the option exists only for backwards
+  # compatibility in RSpec 3). It causes shared context metadata to be
+  # inherited by the metadata hash of host groups and examples, rather than
+  # triggering implicit auto-inclusion in groups with matching metadata.
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+
+# The settings below are suggested to provide a good initial experience
+# with RSpec, but feel free to customize to your heart's content.
+=begin
+  # This allows you to limit a spec run to individual examples or groups
+  # you care about by tagging them with `:focus` metadata. When nothing
+  # is tagged with `:focus`, all examples get run. RSpec also provides
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # Limits the available syntax to the non-monkey patched syntax that is
+  # recommended. For more details, see:
+  # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
+  config.disable_monkey_patching!
+
+  # This setting enables warnings. It's recommended, but in some cases may
+  # be too noisy due to issues in dependencies.
+  config.warnings = true
+
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = "doc"
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+=end
+end

vifreefly/kimuraframework

spec/kimurai/base_spec.rb
@@ -1,0 +1,133 @@
+RSpec.describe Kimurai::Base do 
+  describe '.running?' do
+    pending
+  end
+
+  describe '.completed?' do
+    pending
+  end
+
+  describe '.failed?' do
+    pending
+  end
+
+  describe '.visits' do
+    pending
+  end
+
+  describe '.items' do
+    pending
+  end
+
+  describe '.update' do
+    pending
+  end
+
+  describe '.add_event' do
+    pending
+  end
+
+  describe '.name' do
+    pending
+  end
+
+  describe '.engine' do
+    pending
+  end
+
+  describe '.pipelines' do
+    pending
+  end
+
+  describe '.start_urls' do
+    pending
+  end
+
+  describe '.config' do
+    pending
+  end
+
+  describe '.logger' do
+    pending
+  end
+
+  describe '.crawl!' do
+    pending
+  end
+
+  describe '.parse!' do
+    pending
+  end
+
+  describe '#browser' do
+    pending
+  end
+
+  describe '#request_to' do
+    pending
+  end
+
+  describe '#console' do
+    pending
+  end
+
+  describe '#storage' do
+    pending
+  end
+
+  describe '#unique?' do
+    pending
+  end
+
+  describe "#save_to" do
+    let(:item) { double('item') }
+    let(:path) { '/path/to/file' }
+    let(:format) { :csv }
+    let(:position) { true }
+    let(:append) { false }
+
+    context "when the Saver instance does not exist" do
+      it "creates a new Saver instance and stores it in the @savers instance variable" do
+        saver = instance_double(Kimurai::Base::Saver)
+        expect(saver).to receive(:save).with(item)
+        expect(Kimurai::Base::Saver).to receive(:new).with(path, format: format, position: position, append: append).and_return(saver)
+        allow(described_class).to receive(:savers).and_return({})
+
+        subject.save_to(path, item, format: format, position: position, append: append)
+
+        expect(subject.instance_variable_get(:@savers)[path]).to eq(saver)
+        expect(subject.class.savers[path]).to eq(nil)
+      end
+
+      context "when the with_info class method returns true" do
+        it "stores the Saver instance in the savers class variable" do
+          saver = instance_double(Kimurai::Base::Saver)
+          expect(saver).to receive(:save).with(item)
+          expect(subject).to receive(:with_info).and_return(true)
+          expect(Kimurai::Base::Saver).to receive(:new).with(path, format: format, position: position, append: append).and_return(saver)
+          allow(described_class).to receive(:savers).and_return({})
+
+          subject.save_to(path, item, format: format, position: position, append: append)
+
+          expect(subject.instance_variable_get(:@savers)[path]).to eq(saver)
+          expect(subject.class.savers[path]).to eq(saver)
+        end
+      end
+    end
+
+    context "when the Saver instance already exists" do
+      it "does not create a new Saver instance" do
+        saver = instance_double(Kimurai::Base::Saver)
+        expect(saver).to receive(:save).with(item)
+        subject.instance_variable_set(:@savers, { path => saver })
+        expect(Kimurai::Base::Saver).not_to receive(:new)
+        subject.save_to(path, item, format: format, position: position, append: append)
+      end
+    end
+  end
+
+
+  describe '#add_event' do
+    pending
+  end
+end

spec/kimurai/cli_spec.rb
@@ -1,0 +1,61 @@
+require 'Kimurai/cli'
+
+RSpec.describe Kimurai::CLI do
+  describe "#generate" do
+    context "when generator_type is 'project'" do
+      it "generates a new project with the given name" do
+        expect_any_instance_of(Kimurai::CLI::Generator).to receive(:generate_project).with("foo")
+        subject.generate("project", "foo")
+      end
+
+      it "raises an error if no project name is provided" do
+        expect { subject.generate("project") }.to raise_error("Provide project name to generate a new project")
+      end
+    end
+
+    context "when generator_type is 'spider'" do
+      it "generates a new spider with the given name" do
+        expect_any_instance_of(Kimurai::CLI::Generator).to receive(:generate_spider).with("foo", in_project: false)
+        subject.generate("spider", "foo")
+      end
+
+      it "raises an error if no spider name is provided" do
+        expect { subject.generate("spider") }.to raise_error("Provide spider name to generate a spider")
+      end
+    end
+
+    context "when generator_type is 'schedule'" do
+      it "generates a new schedule" do
+        expect_any_instance_of(Kimurai::CLI::Generator).to receive(:generate_schedule)
+        subject.generate("schedule")
+      end
+    end
+
+    context "when generator_type is unknown" do
+      it "raises an error" do
+        expect { subject.generate("unknown") }.to raise_error("Don't know this generator type: unknown")
+      end
+    end
+  end
+
+  describe "#setup" do
+    it "sets up the server with the given options" do
+      builder = instance_double(Kimurai::CLI::AnsibleCommandBuilder)
+      expect(Kimurai::CLI::AnsibleCommandBuilder).to receive(:new).with("user_host", {}, playbook: "setup").and_return(builder)
+      expect(builder).to receive(:get).and_return("ls")
+      subject.setup("user_host")
+    end
+  end
+
+  describe "#deploy" do
+    pending
+  end
+
+  describe "#crawl" do
+    pending
+  end
+
+  describe "#parse" do
+    pending
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,102 @@
+require "bundler/setup"
+require "kimurai"
+require "pry-nav"
+
+# This file was generated by the `rspec --init` command. Conventionally, all
+# specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
+# The generated `.rspec` file contains `--require spec_helper` which will cause
+# this file to always be loaded, without a need to explicitly require it in any
+# files.
+#
+# Given that it is always loaded, you are encouraged to keep this file as
+# light-weight as possible. Requiring heavyweight dependencies from this file
+# will add to the boot time of your test suite on EVERY test run, even for an
+# individual file that may not need all of that loaded. Instead, consider making
+# a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need
+# it.
+#
+# See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
+  # have no way to turn it off -- the option exists only for backwards
+  # compatibility in RSpec 3). It causes shared context metadata to be
+  # inherited by the metadata hash of host groups and examples, rather than
+  # triggering implicit auto-inclusion in groups with matching metadata.
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+
+# The settings below are suggested to provide a good initial experience
+# with RSpec, but feel free to customize to your heart's content.
+=begin
+  # This allows you to limit a spec run to individual examples or groups
+  # you care about by tagging them with `:focus` metadata. When nothing
+  # is tagged with `:focus`, all examples get run. RSpec also provides
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # Limits the available syntax to the non-monkey patched syntax that is
+  # recommended. For more details, see:
+  # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
+  config.disable_monkey_patching!
+
+  # This setting enables warnings. It's recommended, but in some cases may
+  # be too noisy due to issues in dependencies.
+  config.warnings = true
+
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = "doc"
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+=end
+end
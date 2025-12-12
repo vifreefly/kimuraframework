ece9bcab747f24eeedd8704c8c638033d66a78f3
Victor Afanasev <vicfreefly@gmail.com>
2025-12-12 18:50:59 +0300

    Remove outdated warning for Kimurai::BrowserBuilder.build

--- lib/kimurai/browser_builder.rb
+++ lib/kimurai/browser_builder.rb
@@ -1,12 +1,6 @@
 module Kimurai
   module BrowserBuilder
     def self.build(engine, config = {}, spider:)
-      if config[:browser].present?
-        raise "++++++ BrowserBuilder: browser option is depricated. Now all sub-options inside " \
-          "`browser` should be placed right into `@config` hash, without `browser` parent key.\n" \
-          "See more here: https://github.com/vifreefly/kimuraframework/blob/master/CHANGELOG.md#breaking-changes-110 ++++++"
-      end
-
       begin
         require "kimurai/browser_builder/#{engine}_builder"
       rescue LoadError => e
